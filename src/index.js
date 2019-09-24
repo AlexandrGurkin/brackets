79769075d3dd99ef92019448e99a0e41c14fad28
Alexandr Gurkin <a.i.gurkin@outlook.com>
2019-09-24 18:01:52 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = new Map(bracketsConfig);
+    let openBrackets = [];
+    for (let i = 0; i < str.length; ++i){
+        if (isBracketsPair(brackets, str[i], openBrackets[0])){
+            openBrackets.shift();
+        } else {
+            openBrackets.unshift(str[i]);
+        }
+    }
+    return openBrackets.length === 0;
 }
+
+function isBracketsPair(map, close, open) {
+    return close === map.get(open)
+}
+
